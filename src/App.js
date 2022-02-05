aeafb741b9d2d7ae2c2700d66806db57b908ee6a
EntornosDAM1 <hgomezv01@educantabria.es>
2022-02-05 22:33:32 +0100

    2 files modified

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import './style.css';
 //Importar componentes
 import ArchivoJugar from './components/ArchivoJugar';
+import Header from './components/Header';
 
 
 export default function App() {
   return (
     <div>
+      <Header/>
+      a
       <ArchivoJugar />
     </div>
   );
