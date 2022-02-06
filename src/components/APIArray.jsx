61826c39a20de15027ddac1341932b56f10f07dd
EntornosDAM1 <hgomezv01@educantabria.es>
2022-02-06 23:20:25 +0100

    1 file modified

--- src/components/APIArray.jsx
+++ src/components/APIArray.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Container,Row,Col,table} from 'react-bootstrap';
 
 class APIArray extends React.Component{
 
@@ -9,8 +10,15 @@ class APIArray extends React.Component{
       tableData:[]
     };
   }
+
+  eventoclick(item) {
+    this.setState({
+      name: item.name,
+      latinname: item.latin_name,
+    });
+  }  
   async componentDidMount(){
-    const response =await fetch('https://zoo-animal-api.herokuapp.com/animals/rand/9');
+    const response = await fetch('https://zoo-animal-api.herokuapp.com/animals/rand/9');
     const responseData = await response.json();
     this.setState({
       tableData : responseData,
@@ -20,10 +28,35 @@ class APIArray extends React.Component{
 
   render(){
     return(
-
+      <div>
+        <h1>Intento de API ARRAY</h1><br/>
+        <Container>
+          <Row>
+            <Col>
+            <table>
+                <tr>
+                  <td>Algo</td>
+                  <td>Algo2</td>
+                  <td>Algo3</td>
+                </tr>
+                <td>
+                    {this.state.tableData.map((item)=>{
+                        return (
+                          <tr onClick={() => this.eventoclick(item)}>
+                          <td>{item.name}</td>
+                          <br/>
+                          aaaaaaa
+                                                
+                        </tr>
+                        );
+                    })}
+                </td>
+             </table> 
+            </Col>
+          </Row>  
+        </Container>
+      </div>
     );
-  })
-
-
+  }
 }
 export default APIArray;
